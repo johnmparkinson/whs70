14cf6aeeb8e32c998b64df8f9f4ae2f0ffebae0e
John Parkinson <johnparkinson@Johns-iMac.local>
2019-06-23 19:37:03 -0500

    start project

---
+++ gulpfile.js
@@ -0,0 +1,141 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
+const cache = require('gulp-cache');
+const browserSync = require('browser-sync');
+const cssnano = require('gulp-cssnano');
+const del = require('del');
+const minify = require('gulp-babel-minify');
+
+/* To Do */
+// Clean
+// Inject
+
+let mode = process.env.NODE_ENV || "preview";
+
+let destRoot = (mode == "preview") ? "_tmp" : "_dist";
+
+const paths = {    
+    scripts: {
+      src: "src/js/*.js",
+      dest: destRoot + "/js"
+    },
+    styles: {
+        src: "src/scss/main.scss",
+        dest: destRoot + "/css"
+    },
+    markup: {
+        src: "src/**/*.html",
+        dest: destRoot
+    },
+    images: {
+        src: "src/img/**/*.+(png|jpg|gif|svg)",
+        dest: destRoot + "/img"
+    },
+    data: {
+        src: "src/data/**/*.json",
+        dest: destRoot + "/data"
+    },
+    assets: {
+        src: "src/**/*.+(pdf|xml|webmanifest|txt|ico)",
+        dest: destRoot
+    }
+};
+
+const server = browserSync.create({});
+
+const serve = (done) => {
+    server.init({
+        server: {
+            baseDir: "_tmp/"
+        },
+        serveStatic: ['_tmp'],
+        serveStaticOptions: {
+            extensions: ['html']
+        },
+        middleware: function(req,res,next) {
+            r = /^\/physicians\/physician/g;
+            if (r.test(req.url)) {
+                req.url = '/physicians/physician.html';
+            }
+            return next();
+        }
+    });
+    done();
+}
+
+const compileMarkup = () => {
+    return gulp.src(paths.markup.src)
+        .pipe(gulp.dest(paths.markup.dest))
+}
+
+const compileStyle = () => {
+    return gulp.src(paths.styles.src)
+        .pipe(sass())
+        .pipe(cssnano())
+        .pipe(gulp.dest(paths.styles.dest))
+}
+
+const compileScript = () => {
+    if (mode==="preview") {
+        return gulp.src(paths.scripts.src)    
+        .pipe(babel())
+        .pipe(minify())
+        .pipe(gulp.dest(paths.scripts.dest))        
+    } else {
+        return gulp.src(paths.scripts.src)    
+        .pipe(babel())
+        .pipe(minify())
+        .pipe(gulp.dest(paths.scripts.dest))        
+    }
+}
+
+const compileImages = () => {
+    return gulp.src(paths.images.src)
+        .pipe(cache(imagemin({progressive:true, interlaced:true})))
+        .pipe(gulp.dest(paths.images.dest))
+}
+
+const compileData = () => {
+    return gulp.src(paths.data.src)
+        .pipe(gulp.dest(paths.data.dest))
+}
+
+const compileAssets = () => {
+    return gulp.src(paths.assets.src)
+        .pipe(gulp.dest(paths.assets.dest))
+}
+
+const clean = (done) => {
+    del.sync(destRoot);
+    done();
+}
+
+const watch = (done) => {
+    const watchMarkup = gulp.watch(paths.markup.src, gulp.series(compileMarkup, server.reload));
+    const watchStyle = gulp.watch(paths.styles.src, gulp.series(compileStyle, server.reload));
+    const watchScript = gulp.watch(paths.scripts.src, gulp.series(compileScript, server.reload));
+    const watchImages = gulp.watch(paths.images.src, gulp.series(compileImages, server.reload));
+    const watchData = gulp.watch(paths.data.src, gulp.series(compileData, server.reload));
+    const watchAssets = gulp.watch(paths.assets.src, gulp.series(compileAssets, server.reload));
+    done();
+};
+
+const compile = gulp.parallel(
+    compileMarkup, compileStyle, compileScript, compileImages, compileData, compileAssets
+);
+
+gulp.task('default', gulp.series(clean, compile, serve, watch));
+gulp.task('build', gulp.series(clean, compile));
+
+function reload(done) {
+    server.reload();
+    done();
+}
+
+
+
+
+
+
---
+++ src/js/vue-components.js
@@ -0,0 +1,173 @@
+﻿if (!window.moment) { window.moment = function (str) { return str; }; }
+
+var eventBus = new Vue({});
+
+Vue.component('people-listing', {
+    template: '#people-template',
+    data: function () {
+        return {
+            people: []
+        }
+    },
+    created: function () {
+        var that = this;
+
+        fetch('/data/people.json')
+            .then(results => results.json())
+
+            .then(data => {
+                this.people = data;
+            });
+    }
+});
+
+Vue.component('person', {
+    template: '#person-template',
+    props: ['person'],
+    computed: { 
+        originalUrl : function() {
+            return '../img/1970/' + person.id + '.jpg';
+        },
+        currentUrl : function() {
+            return '../img/current/' + person.id + '.jpg';
+        }
+    }
+});
+
+Vue.component('sort-selector', {
+    template: "#sort-selector",
+    props: ['sorton'],
+    data: function () {
+        return {
+            items: [
+                { id: "date", name: "Date" },
+                { id: "name", name: "Title" }
+            ],
+            selected: this.$props.sorton || 'name'
+        }
+    },
+    methods: {
+        handleSelection: function (value) {
+            eventBus.$emit('select-sort', value);
+        }
+    }
+});
+
+var sort_filter_truncate = function (data, filter, field, sortOn, overridesortOn, top) {
+
+    var filteredData,
+        filteredLength;
+
+    //filter
+    if (data.length > 0 && filter != '') {
+        if (field) {
+            if (Array.isArray(data[0][field])) {
+                if (filter && field) {
+                    filteredData = data.filter(function (d) {
+                        if (d[field]) {
+                            return d[field].indexOf(filter) > -1;
+                        } else { return false; }
+                    });
+                } else if (field) {
+                    // what case is this?
+                    filteredData = data.filter(d => (d[field].length > 0));
+                }
+            } else {
+                if (filter && field) {
+                    filteredData = data.filter(d => (d[field] === filter));
+                }
+            }
+        }
+        else {
+            filteredData = data;
+        }
+    } else {
+        filteredData = data;
+    }
+
+    filteredLength = filteredData.length;
+
+    //sort
+    if (filteredLength > 1) {
+        if (Array.isArray(filteredData[0][sortOn])) {
+            filteredData.sort(function (a, b) {
+                if (a[sortOn][0] == b[sortOn][0]) {
+                    if (a[sortOn][1] == b[sortOn][1]) {
+                        
+                        return b[sortOn][2] - a[sortOn][2];
+                    } else {
+                        return b[sortOn][1] - a[sortOn][1];
+                    }
+                } else {
+                    return b[sortOn][0] - a[sortOn][0];
+                }
+            });
+        } else {
+            filteredData.sort(function (a, b) {
+                var nameA = a[sortOn].toUpperCase();
+                var nameB = b[sortOn].toUpperCase();
+                if (nameA < nameB) {
+                    return -1;
+                }
+                if (nameA > nameB) {
+                    return 1;
+                }
+                return 0;
+            });
+        }
+
+        if (overridesortOn) {
+            filteredData.sort(function (a, b) {
+                return b[overridesortOn];
+            });
+        }
+    }
+
+    //top
+    if (top) {
+        filteredData = filteredData.slice(0, top);
+    } 
+
+    return { filteredLength: filteredLength, data: filteredData };
+}
+
+var sortbyfield = function (filteredData, sortOn ) {
+    if (filteredData.length > 1) {
+        if (Array.isArray(filteredData[0][sortOn])) {
+            filteredData.sort(function (a, b) {
+                if (a[sortOn][0] == b[sortOn][0]) {
+                    if (a[sortOn][1] == b[sortOn][1]) {
+
+                        return b[sortOn][2] - a[sortOn][2];
+                    } else {
+                        return b[sortOn][1] - a[sortOn][1];
+                    }
+                } else {
+                    return b[sortOn][0] - a[sortOn][0];
+                }
+            });
+        } else {
+            filteredData.sort(function (a, b) {
+                var nameA = a[sortOn].toUpperCase();
+                var nameB = b[sortOn].toUpperCase();
+                if (nameA < nameB) {
+                    return -1;
+                }
+                if (nameA > nameB) {
+                    return 1;
+                }
+                return 0;
+            });
+        }
+    }
+}
+
+var sortUnique = function (arr) {
+    return arr.sort().reduce(function (p, c) {
+        if (p.indexOf(c) < 0) {
+            p.push(c);
+        }
+        return p;
+    }, []);
+}
+
